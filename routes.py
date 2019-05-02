bd97c52ca18063c31fb117586760c87588d059f2
NKSCPredictive <nukescrew@gmail.com>
2019-05-02 12:48:50 +1200

    Added new route, Added jinja2 files(layout, home, header, footer, nav, leaderboard)

--- routes.py
+++ routes.py
@@ -7,4 +7,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "yyet"
+    return render_template("home.html", page_title="Home")
+
+
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template("leaderboard.html", page_title="Leaderboard")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="localhost", port=8080)
