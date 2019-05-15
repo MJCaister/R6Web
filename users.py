3ca88ac7eb6b0546fade7d6d5149a2cbe0287c3d
Matthew Caister <Matthew Caister>
2019-05-15 14:36:45 +1200

    More Login Manager Stuff

--- users.py
+++ users.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+import sqlite3
 
 
 class Login(FlaskForm):
@@ -15,3 +16,22 @@ class Signup(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
+
+
+class User(UserMixin):
+    def __init__(self, username, password):
+        self.username = username.lower()
+        self.password = password
+
+    def is_authenticated():
+        return True
+
+    def is_active():
+        return True
+
+    def get_id(self):
+        conn = sqlite3.connect('db/r6web')
+        cur = conn.cursor()
+        cur.execute('''SELECT id FROM ProfileInformation
+                    WHERE username = "{}"'''.format(self.username))
+        return int(cur.fetchone())
