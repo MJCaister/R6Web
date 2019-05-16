4e1880142f0b01588f5406622b964f46b2ed9ed8
Matthew Caister <Matthew Caister>
2019-05-16 12:48:03 +1200

    Removed LoginManager, started work on profiles

--- routes.py
+++ routes.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, flash, redirect, url_for
-from users import Login, Signup
-from flask_login import LoginManager, login_user, current_user
+from users import SubmitData, Signup
 from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '/trailing_slashes/'
-login = LoginManager(app)
 
 
 @app.route('/')
@@ -20,16 +18,17 @@ def leaderboard():
     return render_template("leaderboard.html", page_title="Leaderboard")
 
 
-@app.route('/profile/')
-def profile():
+@app.route('/profile/<username>')
+def profile(username):
+    conn = sqlite3.connect('db/r6web')
+    cur = conn.cursor()
+    cur.execute()
     return render_template("profile.html", page_title="Profile")
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = Login()
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    form = SubmitData()
     if form.validate_on_submit():
         conn = sqlite3.connect('db/r6web.db')
         cur = conn.cursor()
@@ -42,10 +41,9 @@ def login():
         if un[0] is None or not check_password_hash(pw[0], form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
         print("logged in")
         return redirect(url_for('home'))
-    return render_template('login.html', page_title="Sign In", form=form)
+    return render_template('submitdata.html', page_title="Sign In", form=form)
 
 
 @app.route('/signup', methods=['GET', 'POST'])
--- users.py
+++ users.py
@@ -1,14 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, SelectField, SubmitField
 from wtforms.validators import DataRequired
-from flask_login import UserMixin
-import sqlite3
 
 
-class Login(FlaskForm):
+class SubmitData(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
 
@@ -16,22 +13,3 @@ class Signup(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
-
-
-class User(UserMixin):
-    def __init__(self, username, password):
-        self.username = username.lower()
-        self.password = password
-
-    def is_authenticated():
-        return True
-
-    def is_active():
-        return True
-
-    def get_id(self):
-        conn = sqlite3.connect('db/r6web')
-        cur = conn.cursor()
-        cur.execute('''SELECT id FROM ProfileInformation
-                    WHERE username = "{}"'''.format(self.username))
-        return int(cur.fetchone())
