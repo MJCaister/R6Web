1aa508ce87f60bef21766a6011a2b3e1aadd7e1c
Predictive <nukescrew@gmail.com>
2019-06-12 11:45:39 +1200

    Adding image upload to signup - To add a seperate upload page for users

--- users.py
+++ users.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms.validators import DataRequired, ValidationError
 
 
@@ -15,7 +16,8 @@ class SubmitData(FlaskForm):
 class Signup(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    # image =
+    image = FileField('Profile Image (This can be added later)', validators=[
+    FileAllowed(['jpg', 'png'], 'Images only!')])
     submit = SubmitField('Sign Up')
 
 
