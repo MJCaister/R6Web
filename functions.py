9ca56b5335c7aaec1609b5bc1e7b4aaf254abae8
Matthew Caister <Matthew Caister>
2019-08-01 11:45:53 +1200

    More Leaderboard code

--- functions.py
+++ functions.py
@@ -1,7 +1,9 @@
 import sqlite3
 
 
-def leaderboard():
-    conn = sqlite3.connect('db/r6web.db')
-    cur = conn.cursor()
-    cur.execute('''SELECT ''')
+# def leaderboard():
+conn = sqlite3.connect('db/r6web.db')
+cur = conn.cursor()
+cur.execute('''SELECT * FROM SubmitedData;''')
+allData = cur.fetchall()
+print(allData[0, 3])
