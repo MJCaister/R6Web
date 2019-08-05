60c3b07db975ac513e97527e8c0c88ac8ca69332
16086 <16086>
2019-08-06 11:49:11 +1200

    Leaderboard

--- functions.py
+++ functions.py
@@ -5,3 +5,8 @@ import sqlite3
 conn = sqlite3.connect('db/r6web.db')
 cur = conn.cursor()
 # Credit: Bob for helping with queries
+cur.execute('''SELECT id FROM ProfileInformation;''')
+results = cur.fetchall()
+for player in results:
+    cur.execute('''''')
+ 
